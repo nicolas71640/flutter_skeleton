a6cf699ecdf924d666b0b1e626b0cb9e76fb1513
Nicolas Lemblé <nicolas.lemble@wandercraft.eu>
2022-05-31 13:32:37 +0200

    add screenshot tests

---
+++ frontend/integration_test/screenshots.dart
@@ -0,0 +1,46 @@
+import 'dart:io';
+
+import 'package:departments/features/departmentsViewer/data/models/number_trivia_model.dart';
+import 'package:departments/injection_container.dart';
+import 'package:departments/main.dart';
+import 'package:flutter/foundation.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_test/flutter_test.dart';
+import 'package:integration_test/integration_test.dart';
+import 'package:mockito/mockito.dart';
+
+void main() {
+  setUp(() async {
+    await sl.reset();
+    init();
+    await sl.allReady();
+  });
+
+  final IntegrationTestWidgetsFlutterBinding binding =
+      IntegrationTestWidgetsFlutterBinding();
+
+  testWidgets('screenshot', (WidgetTester tester) async {
+    // Render the UI of the app
+    await tester.pumpWidget(MyApp());
+
+    String platformName = '';
+
+    if (!kIsWeb) {
+      // Not required for the web. This is required prior to taking the screenshot.
+      await binding.convertFlutterSurfaceToImage();
+
+      if (Platform.isAndroid) {
+        platformName = "android";
+      } else {
+        platformName = "ios";
+      }
+    } else {
+      platformName = "web";
+    }
+
+    // To make sure at least one frame has rendered
+    await tester.pumpAndSettle();
+    // Take the screenshot
+    await binding.takeScreenshot('screenshot-$platformName');
+  });
+}

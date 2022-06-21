cd734a9e32a3e7c67fe64f21f61ed536ac3ca521
Nicolas Lemblé <nicolas.lemble@wandercraft.eu>
2022-06-21 21:05:06 +0200

    add forgotten file

---
+++ frontend/integration_test/utils/api_utils.dart
@@ -0,0 +1,13 @@
+import 'package:departments/core/network/network_utils.dart';
+import 'package:dio/dio.dart';
+
+class ApiUtils {
+  final Dio dio = Dio(BaseOptions(baseUrl: NetworkUtils.BASE_URL));
+
+  Stream<void> deleteUser(String email) {
+    return Stream.fromFuture(dio.post(
+      '/auth/devDelete',
+      data: {"email": email},
+    ));
+  }
+}

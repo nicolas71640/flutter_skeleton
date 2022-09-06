3c573f0804bfc0c45aceeee998dbb72ee8e65176
Nicolas Lemblé <nicolas.lemble@wandercraft.eu>
2022-09-06 21:10:06 +0200

    fix credentials test

--- frontend/lib/core/local_data_source/credentials_local_data_source.dart
+++ frontend/lib/core/local_data_source/credentials_local_data_source.dart
@@ -12,6 +12,7 @@ abstract class CredentialsLocalDataSource {
   Stream<void> cacheAccessToken(String accessToken);
   Stream<void> cacheCredentials(
       String email, String accessToken, String refreshToken);
+  Stream<void> cleanCredentials();
 }
 
 const ACCESS_TOKEN = "ACCESS_TOKEN";
@@ -52,6 +53,14 @@ class CredentialsLocalDataSourceImpl implements CredentialsLocalDataSource {
   Stream<void> cacheAccessToken(String accessToken) {
     return Stream.fromFuture(
         flutterSecureStorage.write(key: ACCESS_TOKEN, value: accessToken));
+  }
+
+  @override
+  Stream<void> cleanCredentials() {
+    return Stream.fromFuture(flutterSecureStorage
+        .delete(key: EMAIL)
+        .then((_) => flutterSecureStorage.delete(key: ACCESS_TOKEN))
+        .then((_) => flutterSecureStorage.delete(key: REFRESH_TOKEN)));
   }
 
   Stream<String> _getString(String key) {

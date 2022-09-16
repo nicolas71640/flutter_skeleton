19827f01e1f6b5ac5bc099f5c772613c1345a02d
Nicolas Lemblé <nicolas.lemble@wandercraft.eu>
2022-09-16 13:51:03 +0200

    add cottage_api_service tests

--- frontend/test/features/home/data/datasource/cottage_api_service_test.dart
+++ frontend/test/features/home/data/datasource/cottage_api_service_test.dart
@@ -1,21 +1,54 @@
 import 'package:avecpaulette/features/home/data/datasources/cottage_api_service.dart';
+import 'package:avecpaulette/features/home/data/models/api/cottage_request.dart';
 import 'package:dio/dio.dart';
 import 'package:flutter_test/flutter_test.dart';
 import 'package:mockito/annotations.dart';
-// import 'itinerary_api_service_test.mocks.dart';
+import 'package:mockito/mockito.dart';
+import '../../../../fixtures/fixture_reader.dart';
+import 'cottage_api_service_test.mocks.dart';
 
-//TODO
 @GenerateMocks([Dio])
 void main() {
-  // late MockDio mockDio;
-  // late ItineraryApiService itineraryApiService;
+  late MockDio mockDio;
+  late CottageApiService cottageApiService;
 
-  // setUp(() {
-  //   mockDio = MockDio();
-  //   itineraryApiService = ItineraryApiService(mockDio);
-  // });
+  setUp(() {
+    mockDio = MockDio();
+    cottageApiService = CottageApiService(mockDio);
+  });
 
-  // group("getItinerary", () {
-  //   test("should ????? ", () async {});
-  // });
+  group("getCottage", () {
+    test("should return a CottageResponse list when no error is thrown ",
+        () async {
+      final jsonResponse = fixtureJson("cottage/cottage_ok.json");
+
+      when(mockDio.get(any)).thenAnswer((realInvocation) async => Response(
+          requestOptions: RequestOptions(path: ""),
+          statusCode: 200,
+          data: jsonResponse));
+
+      final cottageResponses =
+          await cottageApiService.getCottages(CottageRequest()).first;
+
+      final cottagesJson = (jsonResponse as List);
+      for (int i = 0; i < cottagesJson.length; i++) {
+        expect(cottageResponses[i].title, cottagesJson[i]["title"]);
+        expect(cottageResponses[i].description, cottagesJson[i]["description"]);
+        expect(cottageResponses[i].imageUrl, cottagesJson[i]["imageUrl"]);
+        expect(cottageResponses[i].price, cottagesJson[i]["price"]);
+        expect(cottageResponses[i].latitude, cottagesJson[i]["latitude"]);
+        expect(cottageResponses[i].longitude, cottagesJson[i]["longitude"]);
+      }
+    });
+
+    test("shoud throw a DioError when an error is thrown", () async {
+      final dioError = DioError(requestOptions: RequestOptions(path: ""));
+
+      when(mockDio.get(any))
+          .thenAnswer((_) async => throw dioError);
+
+      expect(cottageApiService.getCottages(CottageRequest()),
+          emitsError(dioError));
+    });
+  });
 }

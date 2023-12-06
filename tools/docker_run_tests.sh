20589cc4ebdfc562c24aa4ff12e383d93bb938c2
Nicolas Lemblé <nicolas.lemble@gmail.com>
2023-12-06 10:00:03 +0000

    emulator ok

--- tools/docker_run_tests.sh
+++ tools/docker_run_tests.sh
@@ -1,27 +1,5 @@
 #!/bin/bash
 
-startEmulator() {
-    #Start emulator if no already online
-    if [ "`adb shell getprop sys.boot_completed | tr -d '\r' `" != "1" ]; then
-
-        adb start-server; \
-        echo -ne "n\n" | $ANDROID_HOME/cmdline-tools/latest/bin/avdmanager create avd -n first_avd -k "system-images;android-29;google_apis;x86"
-        bash -c 'echo "hw.lcd.density=213" >> $HOME/.android/avd/first_avd.avd/config.ini'
-        bash -c 'echo "hw.lcd.height=1280" >> $HOME/.android/avd/first_avd.avd/config.ini'
-        bash -c 'echo "hw.lcd.width=800" >> $HOME/.android/avd/first_avd.avd/config.ini'
-        bash -c 'echo "hw.mainKeys=no" >> $HOME/.android/avd/first_avd.avd/config.ini'
-
-        $ANDROID_HOME/emulator/emulator -avd first_avd -gpu off -no-window -no-audio -dns-server 8.8.8.8 -skin 800x1280 &
-
-        #Wait for the emulator to boot
-        echo "Waiting For the emulator to be online"
-        while [ "`adb shell getprop sys.boot_completed | tr -d '\r' `" != "1" ] ; do sleep 1; done
-    fi;
-}
-
-set -e
-
-source $FLUTTER_WORKSPACE_DIRECTORY/tools/env.env
 
 startEmulator
 

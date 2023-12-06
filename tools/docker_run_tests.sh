bd1f8bc19bf0ead21f96cdb568db1836df83800a
Nicolas Lemblé <nicolas.lemble@gmail.com>
2023-12-06 15:40:00 +0000

    Fix network issues

--- tools/docker_run_tests.sh
+++ tools/docker_run_tests.sh
@@ -1,6 +1,8 @@
-#!/bin/bash
 
+
+source $FLUTTER_WORKSPACE_DIRECTORY/tools/env.env
 
 startEmulator
 
-(cd $FLUTTER_FRONTEND_WORKSPACE_DIRECTORY && flutter test integration_test/tests --verbose --dart-define=BASE_URL=http://10.0.2.2:3000/api --dart-define=FIREBASE_ENABLED=false)
+set -e
+(cd $FLUTTER_FRONTEND_WORKSPACE_DIRECTORY && flutter test integration_test/tests --verbose --dart-define=BASE_URL=$SERVER_BASE_URL --dart-define=FIREBASE_ENABLED=false)

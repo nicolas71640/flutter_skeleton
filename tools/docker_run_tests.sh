3233cd755a8e034883cbd3f8c89655eaca8c7581
Nicolas Lemblé <nicolas.lemble@gmail.com>
2023-12-18 20:34:51 +0000

    fixed ip

--- tools/docker_run_tests.sh
+++ tools/docker_run_tests.sh
@@ -5,4 +5,4 @@ source $FLUTTER_WORKSPACE_DIRECTORY/tools/env.env
 startEmulator
 
 set -e
-(cd $FLUTTER_FRONTEND_WORKSPACE_DIRECTORY && flutter test integration_test/tests --verbose --dart-define=BASE_URL=$SERVER_BASE_URL --dart-define=FIREBASE_ENABLED=false)
+(cd $FLUTTER_FRONTEND_WORKSPACE_DIRECTORY && flutter test integration_test/tests --verbose --dart-define=BASE_URL=http://172.20.0.4:3000/api --dart-define=FIREBASE_ENABLED=false)
